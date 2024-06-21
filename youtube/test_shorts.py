bc8e521d99338adbe0941612a2de51a80758db72
Ruah Kim <kimhk0315@gmail.com>
2024-06-21 11:05:18 +0900

    🎨 검색 실험

--- youtube/test_shorts.py
+++ youtube/test_shorts.py
@@ -42,6 +42,8 @@
 
 
 
+
+
 #api_key='db31f186-85fc-4d80-a8f7-45b3c4488b1b'
 
 from pinecone import Pinecone, ServerlessSpec
@@ -88,7 +90,7 @@ model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
 
 # 저장된 벡터 조회
 fetched_vector = index.fetch(ids=['video1'])
-print(fetched_vector)
+# print(fetched_vector)
 
 
 # 유사도 검색 함수
@@ -99,7 +101,19 @@ def find_similar_videos(query_text):
     return results['matches']
 
 # 예제 사용
-query_text = '냉동 만두 열 개, 숙주 한 줌, 청양고추 두 개, 대파 1/4 쪽, 고춧가루 한 스푼, 물 600ml, 국간장 반 스푼, 멸치액젓 한 스푼, 코인 육수 두 알, 냉동만두 일곱 개, 계란 하나, 후추.'
+query_text = '양조 간장'
 similar_videos = find_similar_videos(query_text)
 print(similar_videos)
 
+
+
+# 만두 좋아하시는 분들은 주목해 주세요 냉동만두로 간단하면서 칼칼한 만두국을 끓여봤는데 감칠맛이 장난 아니네요 
+# 레시피 시작할게요 냉동 만도 열 개 준비해 주시고 잘 씻은 숙주 한 줌도 준비해 주세요 칼칼한 맛을 위해 청양고추 두 개는 
+# 어슷 썰어주고 대파 1/4 쪽도 잘게 어슷썰어 줄게요 준비한 냄비에 기름 세 스푼 정도 부어주고 만두 색에 살짝 해동되어 
+# 흐물거릴 때까지만 구워 줄게요이 만두 속이 맛에 베이스가 될 거예요 가위로 듬성듬성 잘라주고 살짝 갈색 빛이 돌면 고춧가루
+# 한 스푼 넣어 20초 정도 더 볶아 줄게요 여기에 물 600ml 부어주고 국간장 반 스푼 멸치액젓 한 스푼 코인 육수 두 알 넣어
+# 주세요 냉동만두 일곱 개 넣고 숙주 한 줌 청양고추도 넣어 줄게요 무리 끓기 시작하고 2분 뒤쯤 대파 놓고 계란 하나 깨서 
+# 넣어 주세요 뚜껑 닫고 3분 정도 지나서 계란 익을 때까지만 끓여주면 완성이에요 마무리로 후추 툭툭쳐서 한 입 먹어 보면 
+# 칼칼하면서 만두의 육즙이 입에서 [음악] 폭발하네요 해장도 될 거 같은데
+
+# [{'id': 'video1', 'score': 0.886606753, 'values': []}]
